aee897ef41bf196287fe081be8415d25b5610607
Clement <clement.jean@epitech.eu>
2022-04-01 16:19:19 +0800

    add Sprintf instead of '+' concatenation

--- greet/server/greet_many_times.go
+++ greet/server/greet_many_times.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"strconv"
 
 	pb "github.com/Clement-Jean/grpc-go-course/greet/proto"
 )
@@ -10,10 +10,8 @@ import (
 func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
 	log.Printf("GreetManyTimes function was invoked with %v\n", in)
 
-	firstName := in.FirstName
-
 	for i := 0; i < 10; i++ {
-		res := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := fmt.Sprintf("Hello %s, number %d", in.FirstName, i)
 
 		stream.Send(&pb.GreetResponse{
 			Result: res,
