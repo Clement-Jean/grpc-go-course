b4eacb79eb343c542370a144399c581e253dc56e
Clement <clement.jean@epitech.eu>
2022-06-16 18:32:18 +0800

    use -sha256

--- ssl/ssl.ps1
+++ ssl/ssl.ps1
@@ -17,7 +17,7 @@ $MY_SUBJECT="/CN=$SERVER_CN"
 
 # Step 1: Generate Certificate Authority + Trust Certificate (ca.crt)
 openssl genrsa -passout pass:1111 -des3 -out ca.key 4096
-openssl req -passin pass:1111 -new -x509 -days 365 -key ca.key -out ca.crt -subj "/CN=ca"
+openssl req -passin pass:1111 -new -x509 -sha256 -days 365 -key ca.key -out ca.crt -subj "/CN=ca"
 
 # Step 2: Generate the Server Private Key (server.key)
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
@@ -26,7 +26,7 @@ openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 openssl req -passin pass:1111 -new -key server.key -out server.csr -subj $MY_SUBJECT
 
 # Step 4: Sign the certificate with the CA we created (it's called self signing) - server.crt
-openssl x509 -req -extfile ./ssl.ps1.conf -passin pass:1111 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt 
+openssl x509 -req -extfile ./ssl.ps1.conf -passin pass:1111 -sha256 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt 
 
 # Step 5: Convert the server certificate to .pem format (server.pem) - usable by gRPC
 openssl pkcs8 -topk8 -nocrypt -passin pass:1111 -in server.key -out server.pem
